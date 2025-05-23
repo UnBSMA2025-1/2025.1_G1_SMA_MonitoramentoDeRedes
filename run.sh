415530c133eb39deba7b4d16d8d5e7114b71a92d
yanrdgs-dev <yansantosrodrigues.dev@gmail.com>
2025-05-23 13:00:52 -0300

    feat: alterando run.sh para o maven

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-java -cp "out:libs/jade.jar" jade.Boot -gui \
--agents "monitor_Raiz:agents.MonitorAgent, \
-mitigador_Raiz:agents.MitigatorAgent, \
-ataque1:agents.UserTemplateAgent('192.168.0.66','monitor_Raiz','200'), \
-usuario1:agents.UserTemplateAgent('192.168.0.2','monitor_Raiz','2000')"
+mvn clean compile
+mvn exec:java
+
