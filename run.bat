f524a2ec602492253fccd591b570ffc66508519f
yanrdgs-dev <yansantosrodrigues.dev@gmail.com>
2025-05-26 15:44:29 -0300

    remove: removendo a main para merge

--- run.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-java -cp "out;libs/jade.jar" jade.Boot -gui -agents "monitor_Raiz:agents.MonitorAgent, mitigador_Raiz:agents.MitigatorAgent, ataque1:agents.UserTemplateAgent('192.168.0.66','monitor_Raiz','200'), usuario1:agents.UserTemplateAgent('192.168.0.2','monitor_Raiz','2000')"
