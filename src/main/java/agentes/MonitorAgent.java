420cb2d4566e6300988ae7368d1cdaeac8c8251f
yanrdgs-dev <yansantosrodrigues.dev@gmail.com>
2025-05-26 20:23:29 -0300

    feat: função para detectar tipo de ataque

--- src/main/java/agentes/MonitorAgent.java
+++ src/main/java/agentes/MonitorAgent.java
@@ -8,8 +8,10 @@ import jade.domain.FIPAException;
 import jade.domain.FIPANames;
 import jade.lang.acl.ACLMessage;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class MonitorAgent extends Agent implements MonitorGateway {
+
     private Node node;
 
     protected void setup() {
@@ -50,6 +52,17 @@ public class MonitorAgent extends Agent implements MonitorGateway {
         });
     }
 
+    private String classifyAttack(int count, String payload, String header, String url) {
+        if (count > 100) return "dos-flood";
+        if (count > 10) return "basic-dos";
+        if (header != null && header.contains("keep-alive") && count > 5) return "slowloris";
+        if (url != null && Pattern.matches(".*\" *or *1=1.*", url.toLowerCase())) return "sql-injection";
+        if (payload != null && Pattern.matches(".*<script>.*</script>.*", payload.toLowerCase())) return "xss";
+        if (header != null && url != null && header.toLowerCase().contains("..") && url.contains("/etc/passwd")) return "path-traversal";
+        if (url != null && url.contains(".php") && url.contains("?file=")) return "rfi";
+        return null;
+    }
+
     private void notifyMitigator(String ip) {
         addBehaviour(new OneShotBehaviour() {
             public void action() {
@@ -67,6 +80,28 @@ public class MonitorAgent extends Agent implements MonitorGateway {
                         msg.setConversationId("mitigation-request");
                         System.out.println("[MONITOR] Enviando pedido de bloqueio para agente mitigador...");
                         send(msg);
+                    }
+                } catch (FIPAException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void askForReport(String ip, String type) {
+        addBehaviour(new OneShotBehaviour() {
+            @Override
+            public void action() {
+                try {
+                    DFAgentDescription template = new DFAgentDescription();
+                    ServiceDescription sd = new ServiceDescription();
+                    sd.setType("report");
+                    template.addServices(sd);
+
+                    DFAgentDescription[] result = DFService.search(myAgent, template);
+                    for (DFAgentDescription dfd : result) {
+                        ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+                        msg.addReceiver(dfd.getName());
                     }
                 } catch (FIPAException e) {
                     e.printStackTrace();
