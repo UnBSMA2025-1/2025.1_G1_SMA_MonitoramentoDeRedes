87d36bdd596ffb2387e950fd05505a7baa37a216
Rodrigoatila09 <154346800+Rodrigoatila09@users.noreply.github.com>
2025-05-30 17:52:40 -0300

    Atualizando pontos do sistema para registras logs no logstore

--- src/main/java/agentes/MonitorGateway.java
+++ src/main/java/agentes/MonitorGateway.java
@@ -1,5 +1,9 @@
 package agentes;
+import java.util.List;
+import java.util.Map;
 
 public interface MonitorGateway {
     void receiveRequest(String ip);
+    Map<String, Integer> getRequestCountsPerIp();
+    List<RequestRecord> getRecentRequests();
 }
